038addf998985efacc6e4910c9e1d9e52710d1a9
Kim-Dong-Hyeon <dhkim960206@gmail.com>
2024-07-16 14:18:57 +0900

    Feat: Level 1 구현

---
+++ ContactsApp/TableViewCell.swift
@@ -0,0 +1,74 @@
+//
+//  TableViewCell.swift
+//  ContactsApp
+//
+//  Created by 김동현 on 7/15/24.
+//
+
+import UIKit
+import SnapKit
+
+final class TableViewCell: UITableViewCell {
+  
+  static let id = "TableViewCell"
+  
+  private let profileImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.layer.cornerRadius = 30
+    imageView.layer.borderColor = UIColor.gray.cgColor
+    imageView.layer.borderWidth = 2
+    imageView.clipsToBounds = true
+    imageView.contentMode = .scaleAspectFill
+    return imageView
+  }()
+  private let nameLabel: UILabel = {
+    let label = UILabel()
+    label.font = .boldSystemFont(ofSize: 16)
+    label.textColor = .darkGray
+    return label
+  }()
+  private let phoneNumberLabel: UILabel = {
+    let label = UILabel()
+    label.font = .boldSystemFont(ofSize: 16)
+    label.textColor = .darkGray
+    return label
+  }()
+  
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    configureUI()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func configureUI() {
+    [
+      profileImageView,
+      nameLabel,
+      phoneNumberLabel
+    ].forEach { self.addSubview($0) }
+    
+    profileImageView.snp.makeConstraints {
+      $0.width.height.equalTo(60)
+      $0.leading.equalToSuperview().offset(20)
+      $0.centerY.equalToSuperview()
+    }
+    
+    nameLabel.snp.makeConstraints {
+      $0.leading.equalTo(profileImageView.snp.trailing).offset(20)
+      $0.centerY.equalToSuperview()
+    }
+    
+    phoneNumberLabel.snp.makeConstraints {
+      $0.trailing.equalToSuperview().offset(-20)
+      $0.centerY.equalToSuperview()
+    }
+  }
+  
+  public func configureCell(name: String, phoneNumber: String) {
+    nameLabel.text = name
+    phoneNumberLabel.text = phoneNumber
+  }
+}
--- ContactsApp/ViewController.swift
+++ ContactsApp/ViewController.swift
@@ -6,14 +6,73 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+  
+  private let addButton = UIButton(type: .system)
+  private lazy var tableView: UITableView = {
+    let tableView = UITableView()
+    tableView.delegate = self
+    tableView.dataSource = self
+    tableView.register(TableViewCell.self, forCellReuseIdentifier: TableViewCell.id)
+    return tableView
+  }()
+  
+  // 임시 데이터
+  private let friends = [
+    ("가나다", "010-1111-2222"),
+    ("라마바", "010-3333-4444"),
+    ("사아자", "010-5555-6666"),
+    ("차카타", "010-7777-8888"),
+    ("파하", "010-9999-0000")
+  ]
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    configureUI()
   }
-
-
+  
+  private func configureUI() {
+    view.backgroundColor = .white
+    
+    // 네비게이션 타이틀 설정
+    self.title = "친구 목록"
+    
+    // 추가 버튼 설정
+    addButton.setTitle("추가", for: .normal)
+    addButton.addTarget(self, action: #selector(addButtonTapped), for: .touchUpInside)
+    navigationItem.rightBarButtonItem = UIBarButtonItem(customView: addButton)
+    
+    [tableView].forEach { view.addSubview($0) }
+    
+    tableView.snp.makeConstraints {
+      $0.edges.equalTo(view.safeAreaLayoutGuide).inset(20)
+    }
+  }
+  @objc private func addButtonTapped() {
+    // 추가 화면으로 이동
+  }
 }
 
+extension ViewController: UITableViewDelegate {
+  // 테이블 뷰 셀 높이 크기 지정.
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    80
+  }
+}
+
+extension ViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.id) as? TableViewCell else {
+      return UITableViewCell()
+    }
+    let friend = friends[indexPath.row]
+    cell.configureCell(name: friend.0, phoneNumber: friend.1)
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    friends.count
+  }
+}
